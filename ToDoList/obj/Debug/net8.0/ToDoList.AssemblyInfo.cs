7e1929acf0c532edab55908342a58344694425c1
Quinn Le <quynhnle135@gmail.com>
2024-10-22 18:15:02 -0400

    Integrated TailwindCSS into Blazor app

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca662a96616825a24c562a9599c4de7ca1019f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd797d88b480da3ce6e60351e8974aa93fe5be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
