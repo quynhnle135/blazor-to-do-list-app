f881ad5ad0c20d910c923f8bff369d82cf9c28e3
Quinn Le <quynhnle135@gmail.com>
2024-10-23 12:50:17 -0400

    Added Sort by Priority and Status buttons

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd797d88b480da3ce6e60351e8974aa93fe5be2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1929acf0c532edab55908342a58344694425c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
